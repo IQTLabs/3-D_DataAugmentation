d6442de6cf19078afab656679c1a3649a06f2ea0
Michael Lomnitz <mllomnitz@gmail.com>
2020-09-28 14:47:56 -0400

    Adds GAN_train to init

--- p2p/__init__.py
+++ p2p/__init__.py
@@ -2,5 +2,7 @@
 from .dataset import *
 from .models import *
 from .train import *
+from.GAN_train import *
 
-__all__ = [*dataset.__all__, *models.__all__, *train.__all__]
+__all__ = [*dataset.__all__, *models.__all__,
+           *train.__all__, *GAN_train.__all__]
