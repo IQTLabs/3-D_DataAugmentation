0c3b8d39ffe1522d80930c42c3f87b8793782f8a
Michael Lomnitz <mllomnitz@gmail.com>
2020-10-01 13:00:21 -0400

    Adds utils to init

--- p2p/__init__.py
+++ p2p/__init__.py
@@ -2,7 +2,9 @@
 from .dataset import *
 from .models import *
 from .train import *
-from.GAN_train import *
+from .GAN_train import *
+from .utils import *
 
 __all__ = [*dataset.__all__, *models.__all__,
-           *train.__all__, *GAN_train.__all__]
+           *train.__all__, *GAN_train.__all__,
+           *utils.__all__]
