82df9b5fac308e204a35ef47088852fcee6c82e0
Michael Lomnitz <mllomnitz@gmail.com>
2020-10-01 12:36:17 -0400

    Adds preprocessing script for dataset

---
+++ preprocess.py
@@ -0,0 +1,116 @@
+import sys
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
+from skimage.draw import circle, line, line_aa, polygon
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
+import argparse
+
+import torch
+
+import face_alignment
+
+sys.path.insert(0, '/home/mlomnitz/Documents/Projects/i2ai/')
+sys.path.insert(
+    0, '/home/mlomnitz/Documents/Projects/Pose2Pose/everybody_dance_now_pytorch/models')
+from networks import *
+from mmid.utils import AverageMeter, createOptim, VideoReader
+
+device = 'cuda:0'
+out_dir = '/home/mlomnitz/Documents/Data/p2p'
+video_path = '/home/mlomnitz/Documents/Data/VoxCeleb/video_data/voxceleb2'
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+COLORS = [[255, 0, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [0, 255, 255],
+          [0, 170, 255], [0, 0, 255], [170, 0, 255], [255, 0, 255]]
+
+pose_joints = {'face': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+               'l_brow': [17, 18, 19, 20, 21],
+               'r_brow': [22, 23, 24, 25, 26],
+               'bridge': [27, 28, 29, 30],
+               'nostrils': [31, 32, 33, 34, 35],
+               'l_eye': [36, 37, 38, 39, 40, 41, 36],
+               'r_eye': [42, 43, 44, 45, 46, 47, 42],
+               'outer_mouth': [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 48],
+               'inner_mouth': [60, 61, 62, 63, 64, 65, 66, 67, 60]}
+
+fan = face_alignment.FaceAlignment(
+    face_alignment.LandmarksType._3D, flip_input=False, device=device)
+vr = VideoReader()
+
+parser = parser = argparse.ArgumentParser(description='Batch inference script')
+parser.add_argument('--start', dest='start', type=int, default=0)
+parser.add_argument('--end', dest='end', type=int, default=0)
+
+
+def draw_pose_from_cords(face_preds, img_size, radius=2, draw_joints=True):
+    colors = np.zeros(shape=img_size + (3, ), dtype=np.uint8)
+    mask = np.zeros(shape=img_size, dtype=bool)
+    preds = np.asarray(face_preds).reshape(68, 3).astype(np.uint8)
+    for idx, v in enumerate(pose_joints.values()):
+        #
+        for i_pt in range(len(v)-1):
+            yy, xx = line(preds[v[i_pt], 1], preds[v[i_pt], 0],
+                          preds[v[i_pt+1], 1], preds[v[i_pt+1], 0])
+            colors[yy, xx] = COLORS[idx]
+            mask[yy, xx] = True
+            yy, xx = circle(
+                preds[v[i_pt], 1], preds[v[i_pt], 0], radius=radius, shape=img_size)
+            colors[yy, xx] = COLORS[idx]
+            mask[yy, xx] = True
+
+        yy, xx = circle(preds[v[-1], 1], preds[v[-1], 0],
+                        radius=radius, shape=img_size)
+        colors[yy, xx] = COLORS[idx]
+        mask[yy, xx] = True
+
+    return colors, mask
+
+
+def save_frames(frames, save_path):
+    for idx in range(frames.shape[0]):
+        im = Image.fromarray(frames[idx])
+        im.save('{}/frame_{}.jpg'.format(save_path, idx))
+
+
+def save_poses(lmk, save_path):
+    for idx in range(len(lmk)):
+        colors, mask = draw_pose_from_cords(lmk[idx], (224, 224))
+        im = Image.fromarray(colors)
+        im.save('{}/pose_{}.jpg'.format(save_path, idx))
+
+def predict_on_video_set(df, num_workers=5):
+
+    def process_video(i):
+        entry = df.iloc[i]
+        save_path = '{}/{}/{}'.format(out_dir, entry['name'], entry['snippet'])
+        frames, frame_id = vr.read_frames(
+            '{}/{}/{}'.format(video_path, entry['name'], entry['snippet']), 5)
+        frames_t = torch.tensor(frames).permute(0, 3, 1, 2)
+        try:
+            lmk = fan.get_landmarks_from_batch(frames_t.cuda())
+        except:
+            pass
+        Path(save_path).mkdir(parents=True, exist_ok=True)
+        save_frames(frames, save_path)
+        save_poses(lmk, save_path)
+   
+    with ThreadPoolExecutor(max_workers=5) as ex:
+        tqdm(ex.map(process_video, range(len(df))), total=len(df)) 
+
+
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    df = pd.read_csv(
+        '/home/mlomnitz/Documents/Projects/Pose2Pose/snippets_df.csv')
+    predict_on_video_set(df[args.start:args.end])
