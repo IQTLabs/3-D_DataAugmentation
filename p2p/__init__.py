2a051da1f3a629c986cfe87e24d11ac61c8fb554
Michael Lomnitz <mllomnitz@gmail.com>
2020-12-02 16:59:45 -0500

    Adds videoreader utility

--- p2p/__init__.py
+++ p2p/__init__.py
@@ -4,7 +4,8 @@ from .models import *
 from .train import *
 from .GAN_train import *
 from .utils import *
+from .video_reader import *
 
 __all__ = [*dataset.__all__, *models.__all__,
            *train.__all__, *GAN_train.__all__,
-           *utils.__all__]
+           *utils.__all__, *video_reader.__all__]
